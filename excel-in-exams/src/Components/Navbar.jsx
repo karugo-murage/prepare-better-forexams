ff3b844987337babc4ccc1ed4901b094e6f66ecb
karugo-murage <murage.gidion@moringaschool.com>
2022-07-26 12:31:34 +0300

    add feture navbar

--- excel-in-exams/src/Components/Navbar.jsx
+++ excel-in-exams/src/Components/Navbar.jsx
@@ -4,10 +4,14 @@ import Units from './Units'
  export default function Navbar() {
    return (
      <div>
-        <Units unit= "Artificial Intelligence"/>
-        <Units unit= "Integral Calculus"/>
-        <Units unit= "Computer Architecture"/>
-        <Units unit= "Computer Networks"/>
+        <div className='navbar'>
+            <h1 className='success'>SUCCESS IN YOUR EXAMS .</h1>
+            <Units unit= "Artificial Intelligence"/>
+            <Units unit= "Integral Calculus"/>
+            <Units unit= "Computer Architecture"/>
+            <Units unit= "Computer Networks"/>
+        </div>
+        
      </div>
    )
  }
