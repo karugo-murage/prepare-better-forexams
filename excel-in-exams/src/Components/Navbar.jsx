75b580fc8ad2a55307f692186094ea885650bddd
karugo-murage <murage.gidion@moringaschool.com>
2022-07-26 12:07:43 +0300

    add feature buttons and style

--- excel-in-exams/src/Components/Navbar.jsx
+++ excel-in-exams/src/Components/Navbar.jsx
@@ -1,1 +1,14 @@
+ import React from 'react'
+import Units from './Units'
+ 
+ export default function Navbar() {
+   return (
+     <div>
+        <Units unit= "Artificial Intelligence"/>
+        <Units unit= "Integral Calculus"/>
+        <Units unit= "Computer Architecture"/>
+        <Units unit= "Computer Networks"/>
+     </div>
+   )
+ }
  
---
+++ excel-in-exams/src/Components/Units.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Units(props) {
+  return (
+    <button className='examined-units' >{props.unit}</button>
+  )
+}
