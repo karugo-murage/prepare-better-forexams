edf1611fbb3fdad693f912148fe11345ac5215b8
karugo-murage <murage.gidion@moringaschool.com>
2022-07-27 09:15:36 +0300

    insert component Quotes' style

--- excel-in-exams/src/Components/Quotes.js
+++ excel-in-exams/src/Components/Quotes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Quotes() {
   return (
-    <div>
+    <div className='quote-section'>
        <p>Get yourself motivated </p>
        <hr></hr>
        <hr></hr>
