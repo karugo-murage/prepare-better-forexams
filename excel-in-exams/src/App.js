3072a743f099625ffca90338c5ba4cbd4ad37ac3
karugo-murage <murage.gidion@moringaschool.com>
2022-07-27 09:04:49 +0300

    add feature Quotes

--- excel-in-exams/src/App.js
+++ excel-in-exams/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar'
+import Quotes from './Components/Quotes'
 import Theme from './Components/Theme'
+import Comment from './Components/Comment'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Theme/>
+      <Quotes/>
     </div>
   );
 }
---
+++ excel-in-exams/src/Components/Quotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Quotes() {
+  return (
+    <div>
+       <p>Get yourself motivated </p>
+       <hr></hr>
+       <hr></hr>
+    </div>
+  )
+}
