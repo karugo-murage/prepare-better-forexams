00e951ae3c00c8579e99071fb6445c792422efff
karugo-murage <murage.gidion@moringaschool.com>
2022-07-27 08:56:36 +0300

    add and style feature-theme

--- excel-in-exams/src/App.js
+++ excel-in-exams/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar'
+import Theme from './Components/Theme'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Theme/>
     </div>
   );
 }
---
+++ excel-in-exams/src/Components/Theme.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Theme() {
+  return (
+    <div className='theme'>
+    <h2 className='motto'>We are dedicated towards <br/> ensuring  you get the <br/> very best in your exams !</h2>
+    <div className='mantra'>SUCCESS IS OUR MANTRA</div>
+    </div>
+    
+  )
+}
