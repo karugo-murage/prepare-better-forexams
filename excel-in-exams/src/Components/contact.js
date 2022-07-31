ce312d9979e371a5b8968f5371ab192c45575ecb
karugo-murage <murage.gidion@moringaschool.com>
2022-07-31 14:49:55 +0300

    styles component contact

--- excel-in-exams/src/Components/contact.js
+++ excel-in-exams/src/Components/contact.js
@@ -5,13 +5,13 @@ export default function contact() {
     <div>
         <h1 className='hear'>We'd Love to Hear From You</h1>
         <div className='feedback'>
-            <p>Share your feedback on ;</p>
+            <p className='free'>Fell free to share your feedback</p>
             <form >
-                     <input className='input_contact' type="text" placeholder="Enter your name" required/><br/> 
-                     <input className='input_contact' type="text" placeholder="Enter your email" required/> <br/>
-                     <input className='input_contact' type="text" placeholder="Subject" required/><br/>
-                     <textarea  className='input_contact' name="" id="" cols="30" rows="10" placeholder="Message" required>  </textarea> <br/>
-                     <input  className='input_contact' type="submit" class="send"/>
+                <input className='input_contact' type="text" placeholder="Enter your name" required/><br/> 
+                <input className='input_contact' type="text" placeholder="Enter your email" required/> <br/>
+                <input className='input_contact' type="text" placeholder="Subject" required/><br/>
+                <textarea  className='input_contact' name="" id="" cols="30" rows="10" placeholder="Message" required>  </textarea> <br/>
+                <button className='input_send' > Send</button>
             </form>
         </div>
     </div>
