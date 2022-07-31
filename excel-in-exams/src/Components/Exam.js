17c451e4a4e21ee58d6b7e19a013b0f30690ec7b
karugo-murage <murage.gidion@moringaschool.com>
2022-07-31 13:45:44 +0300

    add component contact

--- excel-in-exams/src/Components/Exam.js
+++ excel-in-exams/src/Components/Exam.js
@@ -9,18 +9,18 @@ export default function Exam() {
   const [active, setActive] = useState( dataCom);
   return (
     <>
+     <div>
+    <button className='button' onClick ={()=>setActive(ai)}>A.I</button>
+    <button className='button' onClick ={()=>setActive(calculus)}>Calculus</button>
+    <button className='button' onClick ={()=>setActive(digitalElec)}>Electronics</button>
+    <button className='button' onClick ={()=>setActive(dataCom)}  >Data_Com</button>
+    </div>
     <div>
         { active === digitalElec && <img src={digitalElec}default height={500} width={500} />}
         { active === dataCom&& <img src={dataCom}default height={500} width={500} />}
         { active === calculus && <img src={calculus}default height={500} width={500} />}
         { active === ai && <img src={ai}default height={500} width={500} />}
         
-    </div>
-    <div>
-    <button className='button' onClick ={()=>setActive(ai)}>AI</button>
-    <button className='button' onClick ={()=>setActive(calculus)}>Calculus</button>
-    <button className='button' onClick ={()=>setActive(digitalElec)}>Electroncs</button>
-    <button className='button' onClick ={()=>setActive(dataCom)}  >Data_Com</button>
     </div>
     </>
     
--- excel-in-exams/src/Components/contact.js
+++ excel-in-exams/src/Components/contact.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function contact() {
   return (
-    <div>contact</div>
+    <div>
+        <h1 className='hear'>We'd Love to Hear From You</h1>
+        <div className='feedback'>
+            <p>Share your feedback on ;</p>
+            <form >
+                     <input className='input_contact' type="text" placeholder="Enter your name" required/><br/> 
+                     <input className='input_contact' type="text" placeholder="Enter your email" required/> <br/>
+                     <input className='input_contact' type="text" placeholder="Subject" required/><br/>
+                     <textarea  className='input_contact' name="" id="" cols="30" rows="10" placeholder="Message" required>  </textarea> <br/>
+                     <input  className='input_contact' type="submit" class="send"/>
+            </form>
+        </div>
+    </div>
   )
 }
